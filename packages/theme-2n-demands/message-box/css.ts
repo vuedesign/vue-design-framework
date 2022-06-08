b4fde917dfea6dd3fc270467ce3cc78708d13a2d
n.see <gooarter@gmail.com>
2022-06-08 21:22:20 +0800

    feat:修复 message-box

--- packages/theme-2n-demands/message-box/css.ts
+++ packages/theme-2n-demands/message-box/css.ts
@@ -1,2 +1,5 @@
 import "@vue-design/theme-2n/dist/base.css";
 import "@vue-design/theme-2n/dist/message-box.css";
+import "@vue-design/theme-2n/dist/button.css";
+import "@vue-design/theme-2n/dist/input.css";
+import "@vue-design/theme-2n/dist/overlay.css";
--- packages/theme-2n-demands/message-box/scss.ts
+++ packages/theme-2n-demands/message-box/scss.ts
@@ -1,2 +1,5 @@
 import "@vue-design/theme-2n/src/base.scss";
 import "@vue-design/theme-2n/src/message-box.scss";
+import "@vue-design/theme-2n/src/button.scss";
+import "@vue-design/theme-2n/src/input.scss";
+import "@vue-design/theme-2n/src/overlay.scss";
